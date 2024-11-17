df96d3eb860af33e779ee1efec2eff36bd2804db
Martin Li <gl1768@nyu.edu>
2024-11-17 06:20:17 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@ if (navigationLinks.length > 0) {
   for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener("click", function () {
       for (let i = 0; i < pages.length; i++) {
-        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        if (this.dataset.navLink === pages[i].dataset.page) {
           pages[i].classList.add("active");
           navigationLinks[i].classList.add("active");
           window.scrollTo(0, 0);
